5055dd43f1e3393e2a2ce1014bf7452f957297f1
vinifmatos <viniciusfreire4@gmail.com>
2019-02-24 21:49:25 -0300

    aula 40

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,12 +5,12 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all.includes(:kind, :phones, :address)
 
-    render json: @contacts#, include: %i[kind phones address]
+    render json: @contacts
   end
 
   # GET /contacts/1
   def show
-    render json: @contact
+    render json: @contact, include: %i[kind phones address]
   end
 
   # POST /contacts
@@ -18,7 +18,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
 
     if @contact.save
-      render json: @contact, status: :created, location: @contact
+      render json: @contact, include: %i[kind phones address], status: :created, location: @contact
     else
       render json: @contact.errors, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class ContactsController < ApplicationController
   # PATCH/PUT /contacts/1
   def update
     if @contact.update(contact_params)
-      render json: @contact
+      render json: @contact, include: %i[kind phones address]
     else
       render json: @contact.errors, status: :unprocessable_entity
     end
@@ -39,15 +39,18 @@ class ContactsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def contact_params
-      params.require(:contact).permit(:name, :email, :birthdate, :kind_id,
-      address_attributes: %i[id street city _destroy],
-      phones_attributes: %i[id number _destroy])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def contact_params
+    # params.require(:contact).permit(:name, :email, :birthdate, :kind_id,
+    # address_attributes: %i[id street city _destroy],
+    # phones_attributes: %i[id number _destroy])
+
+    ActiveModelSerializers::Deserialization.jsonapi_parse params
+  end
 end
---
+++ app/serializers/address_serializer.rb
@@ -0,0 +1,4 @@
+class AddressSerializer < ActiveModel::Serializer
+  attributes %i[id street city]
+  belongs_to :contact
+end
