354575e7f4a8a4d4a1c6df4b7ee836fbc3b09df5
= <=>
2019-02-26 17:00:55 +0000

    aulas 45-48 crud rotas aninhadas

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,14 +1,44 @@
 class PhonesController < ApplicationController
-  before_action :set_phones, only: [:show]
+  before_action :set_contact
 
   # GET /phones/1
   def show
-    render json: @phones
+    render json: @contact.phones
+  end
+
+  # POST /contacts/1/phone
+  def create
+    @contact.phones << Phone.new(phone_params)
+    if @contact.save
+      render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
+    else
+      render json: @contact.erros, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /contacts/1/phone
+  def update
+    phone = Phone.find(phone_params[:id])
+    if phone.update(phone_params)
+      render json: @contact.phones
+    else
+      render json: phone.erros, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /contacts/1/phone
+  def destroy
+    phone = Phone.find(phone_params[:id])
+    phone.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_phones
-      @phones = Contact.find(params[:contact_id]).phones
+    def set_contact
+      @contact = Contact.find(params[:contact_id])
+    end
+
+    def phone_params
+      ActiveModelSerializers::Deserialization.jsonapi_parse params
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
     resource :kind, only: :show
     resource :kind, only: :show, path: 'relationships/kind'
 
+    resource :phone, only: %i[create update destroy]
+    resource :phone, only: %i[create update destroy], path: 'relationships/phone'
+
     resource :phones, only: :show
     resource :phones, only: :show, path: 'relationships/phones'
 
-    resource :address, only: :show
-    resource :address, only: :show, path: 'relationships/address'
+    resource :address, except: %i[new edit]
+    resource :address, except: %i[new edit], path: 'relationships/address'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
