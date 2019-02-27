cf91ec5b670db27abd6560cb61acdd819614f6cc
= <=>
2019-02-27 13:10:02 -0300

    aula 66 exigir content type nas requisições

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :ensure_json_request
+  before_action :ensure_json_content_type
 
   def ensure_json_request
-    return if request.headers['Accept'] =~ /json/
+    return if request.headers['Accept'] =~ /vnd\.api\+json/
 
     render nothing: true, status: 406
+  end
+
+  def ensure_json_content_type
+    unless request.get?
+      return if request.headers['Content-Type'] =~ /vnd\.api\+json/
+
+      render nothing: true, status: 415
+    end
   end
 end
