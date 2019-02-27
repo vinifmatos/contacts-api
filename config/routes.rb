5630f835ad3e42dc8a5db9ec61862cff5b728be8
= <=>
2019-02-27 12:40:26 -0300

    aula 64 versionamento api por subdominio

--- config/routes.rb
+++ config/routes.rb
@@ -2,34 +2,40 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # resources :auths, only: %i[create]
   resources :kinds
-  api_version(:module => "V1", :header => {:name => "X-Version", :value => "1"}) do
-    resources :contacts do
-      resource :kind, only: :show
-      resource :kind, only: :show, path: 'relationships/kind'
+  # api_version(:module => "V1", :header => {:name => "X-Version", :value => "1"}) do
+  constraints subdomain: 'v1' do
+    scope module: 'v1'do
+      resources :contacts do
+        resource :kind, only: :show
+        resource :kind, only: :show, path: 'relationships/kind'
 
-      resource :phone, only: %i[create update destroy]
-      resource :phone, only: %i[create update destroy], path: 'relationships/phone'
+        resource :phone, only: %i[create update destroy]
+        resource :phone, only: %i[create update destroy], path: 'relationships/phone'
 
-      resource :phones, only: :show
-      resource :phones, only: :show, path: 'relationships/phones'
+        resource :phones, only: :show
+        resource :phones, only: :show, path: 'relationships/phones'
 
-      resource :address, except: %i[new edit]
-      resource :address, except: %i[new edit], path: 'relationships/address'
+        resource :address, except: %i[new edit]
+        resource :address, except: %i[new edit], path: 'relationships/address'
+      end
     end
   end
-  api_version(:module => "V2", :header => {:name => "X-Version", :value => "2"}) do
-    resources :contacts do
-      resource :kind, only: :show
-      resource :kind, only: :show, path: 'relationships/kind'
+  # api_version(:module => "V2", :header => {:name => "X-Version", :value => "2"}) do
+  constraints subdomain: 'v2' do
+    scope module: 'v2' do
+      resources :contacts do
+        resource :kind, only: :show
+        resource :kind, only: :show, path: 'relationships/kind'
 
-      resource :phone, only: %i[create update destroy]
-      resource :phone, only: %i[create update destroy], path: 'relationships/phone'
+        resource :phone, only: %i[create update destroy]
+        resource :phone, only: %i[create update destroy], path: 'relationships/phone'
 
-      resource :phones, only: :show
-      resource :phones, only: :show, path: 'relationships/phones'
+        resource :phones, only: :show
+        resource :phones, only: :show, path: 'relationships/phones'
 
-      resource :address, except: %i[new edit]
-      resource :address, except: %i[new edit], path: 'relationships/address'
+        resource :address, except: %i[new edit]
+        resource :address, except: %i[new edit], path: 'relationships/address'
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
