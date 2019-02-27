a8eb0709b0bdeedc28bfdd3e2fc1f6846d80b56f
= <=>
2019-02-27 14:11:59 -0300

    aula 69-70 erros padrão jsonapi

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < ApplicationRecord
   belongs_to :kind
   has_many :phones
   has_one :address
+  validates_presence_of :name
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true, allow_destroy: true
   # paginates_per 5
